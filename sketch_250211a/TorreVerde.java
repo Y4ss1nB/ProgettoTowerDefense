94e54846b1aad5928ced54c81b2605422fa18b5d
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-02-18 20:32:39 +0100

    Update TorreVerde.java
    scritto metodo per disegnare la torre triangolare e verde

--- sketch_250211a/TorreVerde.java
+++ sketch_250211a/TorreVerde.java
@@ -3,4 +3,9 @@ class TorreVerde extends Torre{
     public TorreVerde(float x, float y, int raggioAzioneSparo, float tempoRicaricaSparo, float tempoAttesaPerSparare, float potenzaColpo){
         super(x, y, raggioAzioneSparo, tempoRicaricaSparo, tempoAttesaPerSparare, potenzaColpo);
     }
+
+    public void display(){
+        fill(0, 255, 0);
+        triangle(x, y-10, x-10, y+10, x+10, y+10);
+    }
 }
