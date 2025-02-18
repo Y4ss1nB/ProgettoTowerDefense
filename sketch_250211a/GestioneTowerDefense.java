9dd94f11262d21ec9977b6ac437d728ac8955f62
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-02-18 21:31:15 +0100

    Update GestioneTowerDefense.java
    Scritti attributi classe GestioneTowerDefense

--- sketch_250211a/GestioneTowerDefense.java
+++ sketch_250211a/GestioneTowerDefense.java
@@ -1,4 +1,12 @@
-class GestioneTowerDefense {
+import java.util.ArrayList;
+import java.util.Random;
 
-    
+class GestioneTowerDefense {  
+    //attributi
+    ArrayList<Torre> torri;
+    ArrayList<Nemico> nemici;
+    int numeroOndata;
+    int vite;
+    int monete;
+    Random rand;
 }
