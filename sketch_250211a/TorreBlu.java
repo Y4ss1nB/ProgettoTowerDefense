19d750fdb44ccab7e1044047afb4d52430fceacc
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-02-18 20:34:23 +0100

    Update TorreBlu.java
    scritto costruttore per la torre blu

--- sketch_250211a/TorreBlu.java
+++ sketch_250211a/TorreBlu.java
@@ -1,4 +1,6 @@
 class TorreBlu extends Torre{
-    
+    public TorreBlu(float x, float y, int raggioAzioneSparo, float tempoRicaricaSparo, float tempoAttesaPerSparare, float potenzaColpo){
+        super(x, y, raggioAzioneSparo, tempoRicaricaSparo, tempoAttesaPerSparare, potenzaColpo);
+    }
 
 }
