98cd17b0ba5be1ae99abe9166a9b79df1c34aace
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2025-02-18 20:27:21 +0100

    Update TorreVerde.java
    Scritto costruttore della classe verde

--- sketch_250211a/TorreVerde.java
+++ sketch_250211a/TorreVerde.java
@@ -1,3 +1,6 @@
-public class TorreVerde {
-    
+class TorreVerde extends Torre{
+    //costruttore
+    public TorreVerde(float x, float y, int raggioAzioneSparo, float tempoRicaricaSparo, float tempoAttesaPerSparare, float potenzaColpo){
+        super(x, y, raggioAzioneSparo, tempoRicaricaSparo, tempoAttesaPerSparare, potenzaColpo);
+    }
 }
